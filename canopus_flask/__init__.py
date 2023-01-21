3601c95e3154bf888ff1a2dd14f3cd6c1eda8352
Berger-DM <nmmbohnenberger@gmail.com>
2023-01-20 23:57:30 -0300

    Update __init__.py

--- canopus_flask/__init__.py
+++ canopus_flask/__init__.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secretkey'
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///canopusflask.db"
-# app.config['SQLALCHEMY_DATABASE_URI'] =
-# "mysql+mysqlconnector://admin:admin123@database-canopus-003.cok0ixj3xw0j.us-east-1.rds.amazonaws.com:3306/" \
 #
 
 db = SQLAlchemy(app)
